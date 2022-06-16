2259d44a7ceea1ebd7154c991ab624378d831259
Anurag <anuraglama5@gmail.com>
2022-06-16 13:59:27 +0530

    Create Chemicals.h

---
+++ Chemicals.h
@@ -0,0 +1,9 @@
+#include <String.h>
+
+struct hydrogen{
+  int atomicNumber = 1;
+  float atomicMass = 1.0078;
+  float meltingPoint = -259.16;
+  float boilingPoint = -252.87;
+  string type= "Reactive Nonmetal";
+} H;
